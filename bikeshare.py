d77c8c54540ade0d9bd68d7609007f82f35d5b0b
Adem Durdubas <ademdurdubas@Core-M1.fritz.box>
2023-08-07 11:16:25 +0200

    Refactor bikeshare.py 1st Cycle

--- bikeshare.py
+++ bikeshare.py
@@ -18,8 +18,8 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
 
-    # TO DO: get user input for city (chicago, new york city, washington). 
-    # HINT: Use a while loop to handle invalid inputs
+    # get user input for city (chicago, new york city, washington). 
+
 
     try:
         valid_items=['chicago', 'new york city', 'washington']
