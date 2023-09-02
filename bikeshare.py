9b3384c6c830c4da5e49ded8a9ee0ab1820bbcf6
Adem <adurdubas@gmail.com>
2023-09-02 11:42:45 +0200

    Re-Sumbit Change #2

--- bikeshare.py
+++ bikeshare.py
@@ -13,8 +13,8 @@ def get_filters():
 
     Returns:
         (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        (str) month - name of the month to filter by, or "all" to apply no month filter; Entering first 3 chars is also valid
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter; Entering first 3 chars is also valid
     """
     print('Hello! Let\'s explore some US bikeshare data!')
 
